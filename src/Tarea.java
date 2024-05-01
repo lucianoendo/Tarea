cacbc5ef2d34b413b51b34463c3803d3022cf39a
Luciano <lucianotakayama@gmail.com>
2024-05-01 15:25:04 -0500

    este proyecto es la transformación de un registro manual a un programa digital que nos permita llevar el control del consumo de combustible mensual de vehículos terrestres usados dentro de las instalaciones de una empresa con el propósito de movilizar a los trabajadores que lo necesiten

---
+++ src/Tarea.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Tarea{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de empleados que consumieron combustible durante el mes:");
+        int cantidadEmpleados = scanner.nextInt();
+
+        double consumoAnualTotal = 0;
+
+        for (int i = 1; i <= cantidadEmpleados; i++) {
+            System.out.println("Ingrese el consumo de combustible al mes del empleado " + i + " en litros:");
+            double consumoEmpleado = scanner.nextDouble();
+            consumoAnualTotal += consumoEmpleado;
+        }
+
+        double consumoAnualPromedio = consumoAnualTotal / cantidadEmpleados;
+
+        System.out.println("El consumo mensual total de combustible es: " + consumoAnualTotal + " litros");
+        System.out.println("El consumo mensual promedio por empleado es: " + consumoAnualPromedio + " litros");
+
+        scanner.close();
+    }
+}
